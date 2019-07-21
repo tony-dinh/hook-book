b3b72ded7c7c1a1b0647863852aff395efa87d8c
tony-dinh <im.tonydinh@gmail.com>
2019-07-21 11:08:54 -0700

    v0.0.1-beta.17

--- recipes/use-keypress/index.js
+++ recipes/use-keypress/index.js
@@ -22,7 +22,7 @@ export default function useKeyPress() {
             document.removeEventListener('keyup', onKeyUp)
             window.removeEventListener('blur', onBlur)
         }
-    })
+    }, [])
 
     return keyPressed
 }
